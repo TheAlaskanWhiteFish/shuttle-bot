91c7528db7f0320261462ab6f7be411ef7304eb7
TheAlaskanWhiteFish <cmpolloc@mtu.edu>
2017-04-02 14:56:08 -0400

    Update mma8450q.c

--- src/mma8450q/mma8450q.c
+++ src/mma8450q/mma8450q.c
@@ -12,3 +12,17 @@
  #include "../i2c/i2c.h"
  #include "msp430x22x4.h"
  #include "stdint.h"
+
+ UCB0I2CSA = addr;				  // Slave Dev addr w/ SA0=0
+ 
+ while (UCB0STST & UCBBUSY){};    // Carrier Sense
+ UCB0CTL1 |= UCTXSTT + UCTR;      // Tx (ST, Dev addr) + w-bit
+ 
+ UCB0TXBUF = 0x38;               // Tx reg addr of CTRL_REG1
+ while (!(UCB0TXIFG & IFG2)){};   // Wait for Tx_Buf empty
+
+ UCB0TXBUF = 0x0D;                // Tx Data to CTRL_REG1
+ while (!(UCB0TXIFG & IFG2)){};   // Wait for Tx_Buf empty
+
+ UCB0CTL1 |= UCTXSTP;             // Tx Sp after curr byte
+ while (UCB0CTL1 & UCTXSTP){};     // Wait for SP to be sent
